208e475965c3e451af02021dc235e3f1ec8c1f8e
Luch <yupuzu@gmail.com>
2017-11-24 16:21:50 +0200

    use webchannel signals

--- QtLxBTSC/TsWebObject.h
+++ QtLxBTSC/TsWebObject.h
@@ -12,5 +12,10 @@ public:
 	Q_INVOKABLE void emoteClicked(QString e);
 
 signals:
+	void addServer(QString key);
+	void tabChanged(QString key);
+	void textMessageReceived(QString target, QString direction, QString time, QString name, QString message);
+	void statusMessageReceived(QString target, QString time, QString type, QString message);
+	void toggleEmoteMenu();
 	void emoteSignal(QString e);
 };
