f2ebcaa35f99cda92e98cd5499df84e87914fe19
Luch <yupuzu@gmail.com>
2019-12-29 23:21:45 +0200

    add some emote modifiers

--- QtLxBTSC/plugin.cpp
+++ QtLxBTSC/plugin.cpp
@@ -31,7 +31,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "1.13";
+    return "1.14";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
